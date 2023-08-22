9830a3a63cda31b325ca7348e8e241ba044d06bf
RC <rc.dayo@gmail.com>
2023-08-22 14:38:17 -0700

    render random state

--- src/components/Roulette.jsx
+++ src/components/Roulette.jsx
@@ -3,46 +3,91 @@ import { useState } from "react";
 
 import { RestartIcon, StartIcon } from "./Icon";
 
-const options = ['Country', 'State/Province', 'City'];
+const COUNTRY = 'Country';
+const STATE = 'State/Province';
+const CITY = 'City';
+
+const options = [COUNTRY, STATE, CITY];
 
 export default function Roulette({ data }) {
     const [rouletteOption, setRouletteOption] = useState('');
     const [randomPlace, setRandomPlace] = useState('');
+    const [selectedCountry, setSelectedCountry] = useState({});
 
     const countries = data.data;
-    const states = countries.flatMap(country => country.states);
+    const countriesWithStates = countries.filter(country => country.states.length).sort((a, b) => a.name - b.name);
 
-    function handleClick(e) {
-        setRouletteOption(e.target.innerText);
+    function handleGetRandomPlace(e) {
+
+        setRandomPlace('');
+
+        if (rouletteOption === COUNTRY) {
+            const randomCountry = getRandomPlace(countries);
+            setRandomPlace(randomCountry.name);
+        } else if (rouletteOption === STATE) {
+            const countryStates = countriesWithStates.find((country) => country.name === selectedCountry).states;
+            const randomState = getRandomPlace(countryStates);
+            setRandomPlace(randomState.name);
+        }
     };
 
-    function handleRoulette(geography) {
-        let list;
-        console.log(geography);
+    console.log(randomPlace);
 
-        switch (geography) {
-            case 'Country':
-                list = countries;
-                break;
-            case 'State/Province':
-                list = states;
-                break;
-            default:
-                break;
-        }
+    function handleRoulette(e) {
+        e.preventDefault();
 
-        // console.log('list', list);
+        setRandomPlace('');
+        setSelectedCountry('');
+        setRouletteOption('');
 
-        const randomIndex = Math.floor(Math.random() * list.length);
-        setRandomPlace(list[randomIndex]);
+        setRouletteOption(e.target.innerText);
     }
 
+    function handleSelectRegion(e) {
+        e.preventDefault();
+        setSelectedCountry(e.target.innerText);
+        console.log(selectedCountry);
+    }
 
+    function handleRestart(e) {
+        e.preventDefault();
 
-    function handleRestart() {
         setRouletteOption('');
         setRandomPlace('');
     }
+
+    function handleDisableStart() {
+        if (rouletteOption === '') {
+            return true;
+        } else if (rouletteOption === STATE && selectedCountry === '') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const buttonIsDisabled = handleDisableStart();
+
+    const resultsMarkup = randomPlace !== '' && (
+        <div id='results' className="flex flex-col justify-center p-2">
+            <span className="text-xl p-2 text-center">Go to: {randomPlace}!</span>
+            <div className="flex border rounded-xl p-20"></div>
+        </div>
+    );
+
+    const randomStateMarkup = (
+        <div className="flex flex-col p-2 justify-center">
+            <div className="flex flex-col text-center pb-2">
+                Pick a country:
+            </div>
+            <div id='countries-with-states' className="flex flex-wrap justify-center gap-2">
+                {countriesWithStates.map((country, idx) => (
+                    <button onClick={handleSelectRegion} key={idx} className={`p-2 border rounded-lg hover:bg-gray-100 ${country.name === selectedCountry && 'bg-gray-200'}`}>{country.name}</button>
+                ))}
+            </div>
+            {randomPlace !== '' && resultsMarkup}
+        </div>
+    );
 
     return (
         <div className="flex flex-col" >
@@ -51,39 +96,36 @@ export default function Roulette({ data }) {
             <div id='roulette' className="border rounded-lg p-20 flex flex-col">
                 <div id='roulette-options' className="flex justify-center gap-x-2">
                     {options.map((option, index) => (
-                        <button key={index} onClick={handleClick} className={`p-2 border rounded-lg hover:bg-gray-100 ${option === rouletteOption && 'bg-gray-200'}`}>{option}</button>
+                        <button key={index} onClick={handleRoulette} className={`p-2 border rounded-lg hover:bg-gray-100 ${option === rouletteOption && 'bg-gray-200'}`}>{option}</button>
                     ))}
                 </div>
 
                 {/* Buttons */}
                 <div id='roulette-actions' className="flex gap-2 justify-center p-2">
+                    {/* restart */}
                     <button onClick={handleRestart} className={'p-2 border rounded-lg hover:bg-red-500'}>
                         <RestartIcon />
                     </button>
 
-                    <button disabled={rouletteOption === ''} onClick={() => handleRoulette(rouletteOption)} className="p-2 border rounded-xl hover:bg-green-500 ring-sky-500
+                    {/* start */}
+                    <button disabled={buttonIsDisabled} onClick={() => handleGetRandomPlace(rouletteOption)} className="p-2 border rounded-xl hover:bg-green-500 ring-sky-500
       disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200">
                         <StartIcon />
                     </button>
                 </div>
 
                 {/* random country */}
-                {randomPlace && rouletteOption === 'Country' &&
-                    (
-                        <div id='results' className="flex flex-col justify-center p-2">
-                            <span className="text-xl p-2 text-center">Go to: {randomPlace.name}!</span>
-                            <div className="flex border rounded-xl p-20"></div>
-                        </div>
-                    )}
+                {randomPlace && rouletteOption === COUNTRY && resultsMarkup}
 
-                {/* random state / city */}
-                {/* once country is selected, give a dropdown of all states */}
-                {/* OR chooses a random state based on the randomized country  */}
-
+                {/* random state */}
+                {rouletteOption === STATE && randomStateMarkup}
             </div>
         </div >
     );
 }
 
 const iso2FlagEmoji = iso => String.fromCodePoint(...[...iso.toUpperCase()].map(char => char.charCodeAt(0) + 127397));
-// comment random
+
+function getRandomPlace(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
